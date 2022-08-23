5046b398ab723b652651c85b625c9b6e82ab66e4
Martin <martinpspam11@gmail.com>
2022-08-23 17:46:40 -0400

    Allow user to see total score out of five matches, determining an overall winner

--- javascript.js
+++ javascript.js
@@ -16,51 +16,86 @@ function getComputerChoice() {
 
 }
 
+let i = 0;
+
+let playerScore = 0;
+let computerScore = 0;
+
 //Function that will review both inputs and determine a winner
 function playRound(playerSelection) {
-    //Function that will take the string and capitalize the first letter
-    function capitalize(playerSelection) {
-        let fix_string = playerSelection.toLowerCase()
-        return fix_string.charAt(0).toUpperCase() + fix_string.slice(1);
-    }
     
-    let playerSelection_fix = capitalize(playerSelection);
-    let ComputerChoice = getComputerChoice();
-    
-    
-    
-    //Function that will display what happens if you tie
-    function playerTie() {
-        console.log("It was a tie, the computer chose " + ComputerChoice);
-    }
-    //Function that will display what happens if you lose
-    function playerLose() {
-        console.log("You have lost, the computer chose " + ComputerChoice);
-    }
-    //Function that will display what happens if you win
-    function playerWin() {
-        console.log("You have won, the computer chose " + ComputerChoice)
-    }
-    
-    if (playerSelection_fix == ComputerChoice) {
-        playerTie();
-    }
-        else if (playerSelection_fix == "Rock" && ComputerChoice == "Paper") {
-            playerLose();
+    //First five rounds
+    if (i < 5) {
+
+        //Function that will take the string and capitalize the first letter
+        function capitalize(playerSelection) {
+            let fix_string = playerSelection.toLowerCase()
+            return fix_string.charAt(0).toUpperCase() + fix_string.slice(1);
         }
-        else if (playerSelection_fix == "Scissors" && ComputerChoice == "Paper") { 
-            playerWin();
+        
+        let playerSelection_fix = capitalize(playerSelection);
+        let ComputerChoice = getComputerChoice();
+        
+        
+        //Function that will display what happens if you tie
+        function playerTie() {
+            console.log("It was a tie, the computer chose " + ComputerChoice);
+            console.log('Player Score:' + playerScore +  '  ComputerScore:' + computerScore);
+            i++;
+
         }
-        else if (playerSelection_fix == "Rock" && ComputerChoice == "Scissors") {
-            playerWin();
+        //Function that will display what happens if you lose
+        function playerLose() {
+            console.log("You have lost, the computer chose " + ComputerChoice);
+            computerScore++;
+            console.log('Player Score:' + playerScore +  '  ComputerScore:' + computerScore);
+            i++;
         }
-        else if (playerSelection_fix == "Paper" && ComputerChoice == "Scissors") {
-            playerLose();
+        //Function that will display what happens if you win
+        function playerWin() {
+            console.log("You have won, the computer chose " + ComputerChoice);
+            playerScore++ 
+            console.log('Player Score:' + playerScore +  '  ComputerScore:' + computerScore);
+            i++;
         }
-        else if (playerSelection_fix == "Scissors" && ComputerChoice == "Rock") {
-            playerLose();
+        
+        if (playerSelection_fix == ComputerChoice) {
+            playerTie();
         }
-        else if (playerSelection_fix == "Paper" && ComputerChoice == "Rock") {
-            playerWin();
+            else if (playerSelection_fix == "Rock" && ComputerChoice == "Paper") {
+                playerLose();
+            }
+            else if (playerSelection_fix == "Scissors" && ComputerChoice == "Paper") { 
+                playerWin();
+            }
+            else if (playerSelection_fix == "Rock" && ComputerChoice == "Scissors") {
+                playerWin();
+            }
+            else if (playerSelection_fix == "Paper" && ComputerChoice == "Scissors") {
+                playerLose();
+            }
+            else if (playerSelection_fix == "Scissors" && ComputerChoice == "Rock") {
+                playerLose();
+            }
+            else if (playerSelection_fix == "Paper" && ComputerChoice == "Rock") {
+                playerWin();
+            }
+    }       
+        //The response to reveal the overall winner
+        else if(i >= 5){
+                if (playerScore > computerScore) {
+                    console.log('You have won')
+                    console.log('Final Score = Player Score:' + playerScore +  '  ComputerScore:' + computerScore);
+                }
+                    else if (playerScore < computerScore){
+                        console.log('You have lost')
+                        console.log('Final Score = Player Score:' + playerScore +  '  ComputerScore:' + computerScore);
+                    }
+                    else {
+                        console.log('You have tied')
+                        console.log('FInal Score = Player Score:' + playerScore +  '  ComputerScore:' + computerScore);
+                    }
         }
-}       
+}
+
+
