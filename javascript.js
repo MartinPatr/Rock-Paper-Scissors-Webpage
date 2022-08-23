64683f14c444080a37a2307651be98e04a574511
Martin <martinpspam11@gmail.com>
2022-08-22 20:28:25 -0400

    Add function to determine winner with both choices, and add resulting strings after the results of the winner

--- javascript.js
+++ javascript.js
@@ -15,3 +15,50 @@ function getComputerChoice() {
     return ComputerChoice;
 
 }
+
+
+let ComputerChoice = getComputerChoice();
+
+//Function that will display what happens if you tie
+function playerTie() {
+    console.log("It was a tie you both chose, "  + ComputerChoice);
+    getComputerChoice();
+}
+//Function that will display what happens if you lose
+function playerLose() {
+    console.log("You have lost, the computer chose " + ComputerChoice);
+    getComputerChoice();
+}
+//Function that will display what happens if you win
+function playerWin() {
+    console.log("You have won, the computer chose " + ComputerChoice)
+    getComputerChoice();
+}
+
+
+
+//Function that will review both inputs and determine a winner
+function playRound(playerSelection) {
+    let ComputerChoice = getComputerChoice();
+    if (playerSelection == ComputerChoice) {
+        playerTie();
+    }
+        else if (playerSelection == "Rock" && ComputerChoice == "Paper") {
+            playerLose();
+        }
+        else if (playerSelection == "Scissors" && ComputerChoice == "Paper") { 
+            playerWin();
+        }
+        else if (playerSelection == "Rock" && ComputerChoice == "Scissors") {
+            playerWin();
+        }
+        else if (playerSelection == "Paper" && ComputerChoice == "Scissors") {
+            playerLose();
+        }
+        else if (playerSelection == "Scissors" && ComputerChoice == "Rock") {
+            playerLose();
+        }
+        else if (playerSelection == "Paper" && ComputerChoice == "Rock") {
+            playerWin();
+        }
+}       
